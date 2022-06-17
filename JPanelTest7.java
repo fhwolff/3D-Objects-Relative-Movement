5559d4c820bdcd76089d25a27901dd07151a85b9
fhwolff <105082894+fhwolff@users.noreply.github.com>
2022-06-17 15:18:32 -0700

    Add files via upload

---
+++ JPanelTest7.java
@@ -0,0 +1,42 @@
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import java.util.Timer;
+import java.util.TimerTask;
+
+//MOST CURRENT WORKING ONE
+public class JPanelTest7 extends JFrame{
+   
+   static JFrame f = new JPanelTest7();
+   Interface4 screen  = new Interface4();
+   int mouseX;
+   int mouseY;
+   
+   //creates fullscreen panel
+   public JPanelTest7() {
+      add(screen);
+      setUndecorated(true);
+      setSize(Toolkit.getDefaultToolkit().getScreenSize());
+      setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      setVisible(true);
+      
+      // **inputs**
+      //   
+      // ***OUTDATED*** DO NOT USE
+      /*
+      MouseInput2 mouse = new MouseInput2();
+      getContentPane().addMouseListener(mouse);
+      getContentPane().addMouseMotionListener(mouse);
+      mouseX = mouse.mx;
+      mouseY = mouse.my;
+      */
+      
+      /*
+      KeyboardInput2 kboard = new KeyboardInput2();
+      addKeyListener(kboard);
+      */
+   }
+   
+   public static void main(String[] args){
+   
+   }
+} 
